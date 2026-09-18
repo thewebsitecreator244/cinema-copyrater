28d005e2d85288c55f4a157d9184357d04561b31
thewebsitecreator244 <alexkudinov1300@gmail.com>
2026-09-18 14:49:04 +0200

    feat:you cannot have my token stupid hackers i protected it in a .env file

--- src/model/model.ts
+++ src/model/model.ts
@@ -6,8 +6,7 @@ export class Model {
       method: "GET",
       headers: {
         accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzODFmZmU1YTRmZDNlZTI4NmQwNGQ3ODk5MzRhMzkxNiIsIm5iZiI6MTc3NTU2NTU2Ny40MDQsInN1YiI6IjY5ZDRmYWZmYWQ3NTZiYTk0ZTMxM2M5ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4XKIvtuGADbDl7ONjaYZnVTpJYWg45VCzhEniyp2maw",
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
       },
     };
 
